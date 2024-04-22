2519bfd82c8c0f93c610c884fbceab7dbeecc259
Cloudio12 <167665712+Cloudio12@users.noreply.github.com>
2024-04-22 22:51:21 +0800

    Add files via upload

---
+++ data/FasilitasUmumm_1.js
@@ -0,0 +1,13 @@
+var json_FasilitasUmumm_1 = {
+"type": "FeatureCollection",
+"name": "FasilitasUmumm_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nama_Fas": "Pasar Sagerat", "Photo": "C:\\Users\\user\\OneDrive\\Desktop\\tugas webgis\\Foto fasilitas\\pasar sagerat.jpg" }, "geometry": { "type": "Point", "coordinates": [ 125.09845033026555, 1.42896145715143 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nama_Fas": "RSMN Bitung", "Photo": "C:\\Users\\user\\OneDrive\\Desktop\\tugas webgis\\Foto fasilitas\\rs manembo nembo.jpg" }, "geometry": { "type": "Point", "coordinates": [ 125.118489573553987, 1.439899479730312 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Nama_Fas": "Pasar Girian", "Photo": "C:\\Users\\user\\OneDrive\\Desktop\\tugas webgis\\Foto fasilitas\\pasar girian.jpg" }, "geometry": { "type": "Point", "coordinates": [ 125.127040621132579, 1.439038914461737 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Nama_Fas": "Rs BudiMulia", "Photo": "C:\\Users\\user\\OneDrive\\Desktop\\tugas webgis\\Foto fasilitas\\rs budi mulia bitung.jpg" }, "geometry": { "type": "Point", "coordinates": [ 125.178953108900345, 1.445411358843488 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Nama_Fas": "Rs TNI Al", "Photo": "C:\\Users\\user\\OneDrive\\Desktop\\tugas webgis\\Foto fasilitas\\Rs angkatan laut bitung.jpg" }, "geometry": { "type": "Point", "coordinates": [ 125.185925653779933, 1.444766790944079 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Nama_Fas": "Pasar Winenet", "Photo": "C:\\Users\\user\\OneDrive\\Desktop\\tugas webgis\\Foto fasilitas\\pasar winenet.jpg" }, "geometry": { "type": "Point", "coordinates": [ 125.200390834617707, 1.450104374175226 ] } }
+]
+}
